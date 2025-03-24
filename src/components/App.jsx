4a543a2cfbf8797d8146d046bb7d202bc3cc3ded
Patrigar10 <patri.garcia.gomez@gmail.com>
2025-03-24 11:00:37 +0100

    variables de estado

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,52 @@
 import "../scss/App.scss";
+import { useState } from "react";
+
+/*
+Al arrancar la página
+  - Grogu está en la primera celda
+  - Cada caja de mercancías tiene tres elementos
+  - El dado es 0
+
+Funcionalidades
+  -Lanzar el dado. Alhacer click en el dado:
+    - se genera un número aleatorio entre 1 y 4
+    - si es 1 
+      --> Grogu avanza una celda
+    - si es 2 
+      --> vaciamos una galleta
+    - si es 3 
+      --> vaciamos un huevo
+    - si no 
+      --> vaciamos una rana 
+
+    - Si grogu llega a la última celda
+      --> gameover
+    - Si todas las mercancías están vacías
+      --> el jugador gana
+
+  Si la usuaria pulsa "Reiniciar Juego"
+    - Grogu vuelve a la posición 1
+    - Cada una de las cajas de mercancías se llenan
+    - El dado es 0
+
+  Pintar toda la lógica
+        
+        
+*/
+
 
 function App() {
+
+  const [groguPosition, setGroguPosition] = useState("");
+  const [cookies, setCookies] = useState(["🍪", "🍪", "🍪"]);
+  const [eggs, setEggs] = useState(["🥚", "🥚", "🥚"]);
+  const [frogs, setFrogs] = useState(["🐸", "🐸", "🐸"])
+  const [dice, setDice] = useState(0)
+  const [state, setState] = useState(true)
+
+
+  
+
   return (
     <div className="page">
       <header>
